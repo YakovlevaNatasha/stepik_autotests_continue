3b67a64167757717400c81d64fe470717430f380
YakovlevaNatasha <jack.nataly3@yandex.ru>
2019-09-13 13:50:14 +0300

    add time.sleep for check language

--- test_items.py
+++ test_items.py
@@ -1,10 +1,13 @@
-import pytest
 from selenium import webdriver
+import pytest
+import time
 
 
 def test_check_add_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(10)
+
     
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
 
